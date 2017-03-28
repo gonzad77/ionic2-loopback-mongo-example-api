fd50c433e683510a1c132dc480c364fe59cc7e18
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2017-03-28 15:37:12 -0300

    Add email credentials

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -7,5 +7,24 @@ module.exports = {
    password: process.env.DB_PASSWORD,
    database: process.env.DB_NAME,
    url: process.env.DB_URL
+ },
+ emailDs: {
+   name: 'emailDs',
+   connector: 'mail',
+   transports: [
+     {
+       type: 'smtp',
+       host: 'smtp.gmail.com',
+       secure: true,
+       port: 465,
+       tls: {
+         rejectUnauthorized: false
+       },
+       auth: {
+         user: process.env.EMAIL_ADDR,
+         pass: process.env.EMAIL_PASS
+       }
+     }
+   ]
  }
 };
