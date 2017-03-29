fd7c28ddd36c6772fdbedd66a92b7f0d0a1ca5f5
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2017-03-29 15:14:06 -0300

    change for email link verification

--- client/sdk/lb.config.ts
+++ client/sdk/lb.config.ts
@@ -20,7 +20,7 @@
 * }
 **/
 export class LoopBackConfig {
-  private static path: string = '//ionic2-loopback-mongo-api.herokuapp.com';
+  private static path: string = '//0.0.0.0:3000';
   private static version: string | number = 'api';
   private static authPrefix: string = '';
   private static debug: boolean = true;
